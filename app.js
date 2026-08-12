87feab31a4b4a9f45479270113dc4e5a3bd15375
Project for Sanitation Justice @ SDSU <bathrooms.sdsu@gmail.com>
2026-08-11 23:19:27 -0700

    Update map initialization and formatting

--- app.js
+++ app.js
@@ -95,19 +95,19 @@ document.addEventListener("DOMContentLoaded", () => {
      ========================================================= */
 
   const map = L.map("map").setView(
-    [32.9, -117.1],
-    40
-  );
+  [33.05, -116.80],
+  9
+);
 
-  L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-      maxZoom: 19,
-      attribution: "&copy; OpenStreetMap contributors",
-    }
-  ).addTo(map);
+L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution: "&copy; OpenStreetMap contributors",
+  }
+).addTo(map);
 
-  const restroomMarkers = L.layerGroup().addTo(map);
+const restroomMarkers = L.layerGroup().addTo(map);
 
 
   /* =========================================================
