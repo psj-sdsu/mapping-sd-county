96cecdea3b0c52e9bcd8e020033f5f373d548a74
Project for Sanitation Justice @ SDSU <bathrooms.sdsu@gmail.com>
2026-08-11 23:13:23 -0700

    Reduce maxZoom from 20 to 10 in tileLayer

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", () => {
   L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     {
-      maxZoom: 20,
+      maxZoom: 10,
       attribution: "&copy; OpenStreetMap contributors",
     }
   ).addTo(map);
